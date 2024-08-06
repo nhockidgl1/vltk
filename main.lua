6e924fc52c86d285a9dd1eea7de86fc0fe874561
nhockidgl1 <nhockidgl1@gmail.com>
2024-08-07 00:29:36 +0700

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,21 @@
+
+Include("\\script\\global\\vinh\\simcity\\main.lua")
+Include("\\script\\misc\\eventsys\\eventsys.lua")
+
+
+function add_npc_vinh()
+end
+
+--function simcity_addNpcs()
+	-- SimCity: them Trieu Man o 7 thanh
+	SimCityMainThanhThi:addNpcs()
+	
+	-- KeoXe: them VoKy o TuongDuong
+	--add_dialognpc({ 
+		{103,78,1608,3235,"\\script\\global\\vinh\\simcity\\controllers\\keoxe.lua","V« Kþ"}, 
+	})
+
+
+	EventSys:GetType("EnterMap"):Reg("ALL", SimCityMainThanhThi.onPlayerEnterMap, SimCityMainThanhThi)
+	EventSys:GetType("LeaveMap"):Reg("ALL", SimCityMainThanhThi.onPlayerExitMap, SimCityMainThanhThi)
+end
